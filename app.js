7fcd729f16e8a4335ed074d1a76d7b8def345a78
Bruno Sansigolo <contact@masa.solutions>
2017-05-16 15:40:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+var bodyParser =    require("body-parser"),
+    mongoose =      require("mongoose"),
+    express =       require("express"),
+    app     =       express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/messages_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// MONGOOSE/MODEL CONFIG
+// SCHEMA
+var messageSchema = new mongoose.Schema({
+    message: String,
+});
+
+// MODEL
+var Message = mongoose.model("Message", messageSchema);
+
+// REDIRECT TO INDEX
+app.get("/", function(req, res){
+    res.redirect("/messages");
+});
+
+// INDEX - SHOW ALL ID/MESSAGES
+app.get("/messages", function(req, res) {
+    Message.find({}, function(err, foundMessages){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {messages: foundMessages});
+        }
+    });
+});
+
+// CREATE NEW MESSAGE
+app.get("/messages/:message", function(req, res){
+    var msg = req.params.message;
+    Message.create({message: msg}, function(err, newlyMessage){
+        if(err){
+        console.log(err);
+        } else {
+            res.render("new", {newMessage: newlyMessage});
+        }
+    });
+});
+
+// SEARCH MESSAGE BY ID
+app.get("/messages/search/:id", function(req, res) {
+    Message.findById(req.params.id, function(err, foundMessage){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("search", {foundMessage: foundMessage});
+        }
+    });
+});
+
+app.get("/*", function(req, res) {
+    res.redirect("/");
+    console.log("Not implemented yet.");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Server listening...");
+    }
+});
